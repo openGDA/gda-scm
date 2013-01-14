8a1a70f789dddb79a54df472329aac08a69dcf72
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-01-14 17:57:58 +0000

    xls and xlsx import should be working once it compiles
    will currently not compile because of DASCTEST-41

--- plugins/uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/wizards/BSSCImportWizardPage.java
+++ plugins/uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/wizards/BSSCImportWizardPage.java
@@ -12,6 +12,10 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.WorkbookUtil;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
@@ -25,12 +29,16 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import uk.ac.gda.devices.bssc.beans.BSSCSessionBean;
 import uk.ac.gda.devices.bssc.beans.LocationBean;
 import uk.ac.gda.devices.bssc.beans.TitrationBean;
 
 public class BSSCImportWizardPage extends WizardNewFileCreationPage {
+
+	private static final Logger logger = LoggerFactory.getLogger(BSSCImportWizardPage.class);
 
 	protected FileFieldEditor editor;
 	private float sampleStorageTemperature = 15;
@@ -59,7 +67,8 @@ public class BSSCImportWizardPage extends WizardNewFileCreationPage {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				IPath path = new Path(BSSCImportWizardPage.this.editor.getStringValue());
-				setFileName(path.lastSegment());
+				String newFileName = path.removeFileExtension().addFileExtension("xml").lastSegment();
+				setFileName(newFileName);
 			}
 		});
 		String[] extensions = new String[] { "*.xls;*.xlsx" }; // NON-NLS-1
@@ -71,9 +80,22 @@ public class BSSCImportWizardPage extends WizardNewFileCreationPage {
 	protected void createLinkTarget() {
 	}
 	
+	private short parsePlateCell(Cell cell) {
+		if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+			return (short) cell.getNumericCellValue();
+		} 
+		short result = 0;
+		String str = cell.getStringCellValue();
+		for (int i = 0; i < str.length(); i++) {
+			if ("I".equalsIgnoreCase(str.substring(i, i+1))) 
+				result++;
+		}
+		return result;
+	}
+	
 	private LocationBean locationFromCells(Cell platec, Cell rowc, Cell columnc) {
 		LocationBean location = new LocationBean();
-		location.setPlate(Short.parseShort(platec.getStringCellValue()));
+		location.setPlate(parsePlateCell(platec));
 		location.setRow(rowc.getStringCellValue().charAt(0));
 		location.setColumn((short) columnc.getNumericCellValue());
 		return location;
@@ -82,10 +104,8 @@ public class BSSCImportWizardPage extends WizardNewFileCreationPage {
 	@Override
 	protected InputStream getInitialContents() {
 		try {
-			InputStream excelDocumentStream = new FileInputStream(new File(editor.getStringValue()));
-			POIFSFileSystem fsPOI = new POIFSFileSystem(new BufferedInputStream(excelDocumentStream));
-			HSSFWorkbook workBook = new HSSFWorkbook(fsPOI);
-			HSSFSheet sheet = workBook.getSheetAt(0);
+			Workbook wb = WorkbookFactory.create(new File(editor.getStringValue()));
+			Sheet sheet = wb.getSheetAt(0);
 
 			BSSCSessionBean sessionBean = new BSSCSessionBean();
 			sessionBean.setSampleStorageTemperature(sampleStorageTemperature);
@@ -93,26 +113,29 @@ public class BSSCImportWizardPage extends WizardNewFileCreationPage {
 
 			for (Row row : sheet) {
 
-				TitrationBean tibi = new TitrationBean();
-
-				LocationBean location = locationFromCells(row.getCell(0), row.getCell(1), row.getCell(2));
-				tibi.setLocation(location);
-			
-				tibi.setSampleName(row.getCell(3).getStringCellValue());
-
-				location = locationFromCells(row.getCell(4), row.getCell(5), row.getCell(6));
-				tibi.setBufferLocation(location);
-
-//				tibi.setConcentration(concentration); 
-//				tibi.setExposureTemperature(exposureTemperature); 
-//				tibi.setFrames(frames); 
-//				tibi.setRecouperate(recouperate);
-//				tibi.setTimePerFrame(timePerFrame);
-//				tibi.setViscosity(viscosity);
-
-				measurements.add(tibi);
+				try {
+					TitrationBean tibi = new TitrationBean();
+	
+					LocationBean location = locationFromCells(row.getCell(0), row.getCell(1), row.getCell(2));
+					tibi.setLocation(location);
+				
+					tibi.setSampleName(row.getCell(3).getStringCellValue());
+	
+					location = locationFromCells(row.getCell(4), row.getCell(5), row.getCell(6));
+					tibi.setBufferLocation(location);
+	
+					tibi.setRecouperate(row.getCell(7).getBooleanCellValue());
+					tibi.setConcentration(row.getCell(8).getNumericCellValue()); 
+					tibi.setViscosity(row.getCell(9).getStringCellValue());
+					tibi.setTimePerFrame(row.getCell(10).getNumericCellValue());
+					tibi.setFrames((int) row.getCell(11).getNumericCellValue()); 
+					tibi.setExposureTemperature((float) row.getCell(12).getNumericCellValue()); 
+	
+					measurements.add(tibi);
+				} catch (Exception e) {
+					logger.debug("row rejected"+row.toString());
+				}
 			}
-			excelDocumentStream.close();
 
 			sessionBean.setMeasurements(measurements);
 
