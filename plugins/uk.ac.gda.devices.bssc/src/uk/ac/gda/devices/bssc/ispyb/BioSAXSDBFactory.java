cd195c380945e8f39e274c3f89dbeaf67cfb68b1
Karl Levik <karl.levik@diamond.ac.uk>
2013-01-24 16:26:59 +0000

    New classes + slight modifications to use JDBC to store data in DB.

---
+++ plugins/uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/ispyb/BioSAXSDBFactory.java
@@ -0,0 +1,29 @@
+/*-
+ * Copyright © 2011 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.devices.bssc.ispyb;
+
+public class BioSAXSDBFactory {
+	
+	public static BioSAXSISPyB makeDB(BioSAXSISPyB.RDBMSTYPE rdbms, BioSAXSISPyB.MODE mode) {
+		if (rdbms == BioSAXSISPyB.RDBMSTYPE.Oracle) {
+				return new BioSAXSOracleUtils(mode);
+		}
+		return null;
+	}
+}
