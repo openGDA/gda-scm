e536dc6a7318fe38f7c2312ebfe4b16dc93f22df
Karl Levik <karl.levik@diamond.ac.uk>
2013-01-24 16:40:45 +0000

    Use factory to get suitable class.

--- uk.ac.gda.devices.bssc.test/src/uk/ac/gda/devices/bssc/ispyb/BioSAXSDBUtilsTest.java
+++ uk.ac.gda.devices.bssc.test/src/uk/ac/gda/devices/bssc/ispyb/BioSAXSDBUtilsTest.java
@@ -11,29 +11,30 @@ import org.junit.Test;
 
 public class BioSAXSDBUtilsTest {
 
-	static BioSAXSOracleUtils bioSAXSDBUtils = null;
+	static BioSAXSISPyB bioSAXSISPyB = null;
 	
 	@BeforeClass
 	public static void setup() {
-		bioSAXSDBUtils = new BioSAXSOracleUtils(BioSAXSISPyB.MODE.testing);
+		
+		bioSAXSISPyB = BioSAXSDBFactory.makeDB(BioSAXSISPyB.RDBMSTYPE.Oracle, BioSAXSISPyB.MODE.testing);
 	}
 	
 	@Test
 	public void testGetSessionForVisit() throws SQLException {
-		assertEquals(bioSAXSDBUtils.getSessionForVisit("nt20-12"), 434);
+		assertEquals(bioSAXSISPyB.getSessionForVisit("nt20-12"), 434);
 	}
 	
 	@Test
 	public void testcreateMeasurementsAndRegisterBufferForSample() throws SQLException {
-		long bufferId = bioSAXSDBUtils.createBufferMeasurement(434L, (short)0, (short)1, (short)1, 20.0f, 21.0f, 10, 1.0, 2.0, 5.0, 10.0, 
+		long bufferId = bioSAXSISPyB.createBufferMeasurement(434L, (short)0, (short)1, (short)1, 20.0f, 21.0f, 10, 1.0, 2.0, 5.0, 10.0, 
 				"viscosity", "/dls/i22/data/2013/sm999-9/i22-9999.nxs", "/entry1/detector/data");
 		assertTrue(bufferId >= 0);
 
-		long sampleId = bioSAXSDBUtils.createSampleMeasurement(434L, (short)1, (short)1, (short)2, "The blue one",
+		long sampleId = bioSAXSISPyB.createSampleMeasurement(434L, (short)1, (short)1, (short)2, "The blue one",
 				19.0, 20.0f, 21.0f, 10, 1.0, 2.0, 3.0, 11.0, 
 				"viscosity", "/dls/i22/data/2013/sm999-9/i22-9999.nxs", "/entry1/detector/data");
 		assertTrue(sampleId >= 0);
 		
-		bioSAXSDBUtils.registerBufferForSample(434L, sampleId, bufferId);
+		bioSAXSISPyB.registerBufferForSample(434L, sampleId, bufferId);
 	}
 }
