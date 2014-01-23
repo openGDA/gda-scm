c2a0348d761b42ecaf80cb74c6b2bbf0ac121e86
Conor Lehane <xlw00930@ws141.diamond.ac.uk>
2014-01-23 13:22:52 +0000

    Updated EditQueuedCommandHandler to open the editor for the selected item being edited in the queue

--- plugins/uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/ui/handlers/EditQueuedItemHandler.java
+++ plugins/uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/ui/handlers/EditQueuedItemHandler.java
@@ -18,12 +18,38 @@
 
 package uk.ac.gda.devices.bssc.ui.handlers;
 
+import gda.commandqueue.CommandDetails;
+import gda.commandqueue.CommandDetailsPath;
+import gda.commandqueue.Queue;
+import gda.commandqueue.QueuedCommandSummary;
+
+import java.io.File;
+import java.util.List;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.core.expressions.EvaluationContext;
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IStorageEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.gda.client.CommandQueueViewFactory;
+import uk.ac.gda.client.QueueEntry;
+import uk.ac.gda.richbeans.xml.string.StringInput;
+import uk.ac.gda.richbeans.xml.string.StringStorage;
 
 public class EditQueuedItemHandler implements IHandler {
+	private static final Logger logger = LoggerFactory.getLogger(EditQueuedItemHandler.class);
 
 	@Override
 	public void addHandlerListener(IHandlerListener handlerListener) {
@@ -39,12 +65,58 @@ public class EditQueuedItemHandler implements IHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.println("Edit the queued item here");
+		QueuedCommandSummary qCmdSummary = null;
+
+		Queue queue = CommandQueueViewFactory.getQueue();
+
+		EvaluationContext context = (EvaluationContext) event.getApplicationContext();
+		Object obj = context.getDefaultVariable();
+		List<?> list = (List<?>) obj;
+
+		if (list.get(0) instanceof QueueEntry) {
+			final QueueEntry item = (QueueEntry) list.get(0);
+
+			qCmdSummary = item.getQueueCommandSummary();
+		}
+
+		try {
+			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+
+			if (qCmdSummary != null) {
+				CommandDetails commandDetails = queue.getCommandDetails(qCmdSummary.id);
+
+				if (commandDetails instanceof CommandDetailsPath) {
+					String path = ((CommandDetailsPath) commandDetails).getPath();
+
+					File fileToOpen = new File(path);
+
+					try {
+						IFileStore fileStore = EFS.getLocalFileSystem().getStore(fileToOpen.toURI());
+						IEditorPart part = IDE.openEditorOnFileStore(page, fileStore);
+						// part.addPropertyListener(new CommandDetailsEditorListener(tableViewer));
+					} catch (PartInitException e) {
+						logger.error("Error opening file " + path, e);
+					}
+				} else {
+					String description = queue.getCommandSummary(qCmdSummary.id).getDescription();
+					IStorage storage = new StringStorage(commandDetails.getSimpleDetails(), description);
+					IStorageEditorInput input = new StringInput(storage);
+					if (page != null) {
+						IEditorPart part = page.openEditor(input, "org.eclipse.ui.DefaultTextEditor");
+						// part.addPropertyListener(new CommandDetailsEditorListener(tableViewer));
+					}
+				}
+			}
+		} catch (Exception ex) {
+			logger.error("Error showing command details", ex);
+		}
+
 		return null;
 	}
 
 	@Override
 	public boolean isEnabled() {
+		// Need to add condition here so that this menu is only enabled if an experiment is selected in the queue
 		return true;
 	}
 
@@ -58,5 +130,4 @@ public class EditQueuedItemHandler implements IHandler {
 		// TODO Auto-generated method stub
 
 	}
-
 }
