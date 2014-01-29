78d0f80ac5754f8309a98bc5bcd4c1e98ac0d742
Conor Lehane <xlw00930@ws141.diamond.ac.uk>
2014-01-29 10:26:23 +0000

    Renamed BioSAXSSampleProgressCollection to BioSAXSProgressModel

--- plugins/uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/beans/BioSAXSProgressModel.java
+++ plugins/uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/beans/BioSAXSProgressModel.java
@@ -36,20 +36,18 @@ import org.slf4j.LoggerFactory;
 import uk.ac.gda.devices.bssc.ispyb.BioSAXSDBFactory;
 import uk.ac.gda.devices.bssc.ispyb.BioSAXSISPyB;
 
-public class BioSAXSSampleProgressCollection extends ArrayList<ISampleProgress> implements ISampleProgressCollection {
+public class BioSAXSProgressModel extends ArrayList<ISampleProgress> implements IProgressModel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static final Logger logger = LoggerFactory.getLogger(BioSAXSSampleProgressCollection.class);
+	private static final Logger logger = LoggerFactory.getLogger(BioSAXSProgressModel.class);
 	WritableList items = new WritableList(new ArrayList<ISampleProgress>(), ISampleProgress.class);
-	private BioSAXSISPyB bioSAXSISPyB;
 
-	public BioSAXSSampleProgressCollection() {
+	public BioSAXSProgressModel() {
 		//Set up connection to ISpyB
-		new BioSAXSDBFactory().setJdbcURL("jdbc:oracle:thin:@duoserv12.diamond.ac.uk:1521:ispyb");
-		bioSAXSISPyB = BioSAXSDBFactory.makeAPI();
-		pollISpyB();
+		BioSAXSProgressController controller = new BioSAXSProgressController(this);
+		controller.pollISpyB();
 	}
 
 	@Override
@@ -77,51 +75,5 @@ public class BioSAXSSampleProgressCollection extends ArrayList<ISampleProgress> 
 			}
 		});
 
-	}
-
-	@Override
-	public void pollISpyB() {
-		final Job pollingJob = new Job("Polling ISpyB") {
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					clearItems();
-					loadModelFromISPyB();
-					if (monitor.isCanceled())
-						return Status.CANCEL_STATUS;
-
-					return Status.OK_STATUS;
-				} finally {
-					// start job again after specified time has elapsed
-					schedule(90000);
-				}
-			}
-		};
-		// job.addJobChangeListener(new JobChangeAdapter() {
-		// @Override
-		// public void done(IJobChangeEvent event) {
-		// if (event.getResult().isOK())
-		// System.out.println("Job completed successfully");
-		// else
-		// System.out.println("Job did not complete successfully");
-		// }
-		// });
-		pollingJob.schedule(); // start as soon as possible
-	}
-
-	private void loadModelFromISPyB() {
-		String visit;
-
-		try {
-			visit = GDAMetadataProvider.getInstance().getMetadataValue("visit");
-			long blSessionId = bioSAXSISPyB.getSessionForVisit(visit);
-			addItems(bioSAXSISPyB.getBioSAXSMeasurements(blSessionId));
-		}
-		catch (DeviceException e) {
-			logger.error("Device Exception retrieving visit from GDAMetaDataProvider" + e);
-		}
-		catch (SQLException e) {
-			logger.error("SQL Exception getting samples from ISPyB", e);
-		} 
 	}
 }
