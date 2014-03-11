ccdbeee9566761e3e24266ed9f76bce8917b27c2
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2014-03-11 15:34:22 +0000

    SCATTER-235

--- configurations/ncdsim-config/processing/biosaxs.sh
+++ configurations/ncdsim-config/processing/biosaxs.sh
@@ -5,7 +5,7 @@
 module load global/cluster
 
 # those two need to be in sync
-DAWN=/dls_sw/apps/DawnDiamond/master/builds-stable/stable-linux64/dawn
+DAWN=/dls_sw/apps/DawnDiamond/1.4.1/builds-stable/stable-linux64/dawn
 MOML=/home/zjt21856/ncd_model.moml
 
 # those would be found in the environment
@@ -18,24 +18,30 @@ MOML=/home/zjt21856/ncd_model.moml
 #BACKGROUNDFILE=/home/zjt21856/i22-34820.nxs
 DATAFILE="$1"
 BACKGROUNDFILE="$2"
+DATACOLLID="$3"
+
+
+ISPYBUPDATE=$(dirname $0)/updateispyb.py
 
 REDUCTIONOUTPUTFILE=
 
+echo edited file
 #is file in visit
 read VISIT RESTOFPATH <<<$( echo $DATAFILE | sed 's,\(/dls/.../data/20../[-a-z0-9]*\)/\(.*\),\1 \2,')
+RESTOFPATH=${RESTOFPATH%.*}
 echo $RESTOFPATH
 if test -d $VISIT ; then
 	echo running reduction in visit based directory under $VISIT
 	if test -w $VISIT ; then
-		REDUCTIONOUTPUTFILE=$(basename ${VISIT}/processed/${RESTOFPATH} nxs).reduced.nxs
+		REDUCTIONOUTPUTFILE=${VISIT}/processed/${RESTOFPATH}.reduced.nxs
 		mkdir -p $(dirname $REDUCTIONOUTPUTFILE)
 	else 
-		REDUCTIONOUTPUTFILE=$(basename ${VISIT}/processing/${RESTOFPATH} nxs).reduced.nxs
+		REDUCTIONOUTPUTFILE=${VISIT}/processing/${RESTOFPATH}.reduced.nxs
 		mkdir -p $(dirname $REDUCTIONOUTPUTFILE)
 		REDUCTIONOUTPUTFILE=
 	fi
-	TMPDIR=$(dirname ${VISIT}/tmp/${RESTOFPATH})
-	mkdir -p $TMPDIR
+	TMPDIR=${VISIT}/tmp/${RESTOFPATH}
+	mkdir -p $(dirname $TMPDIR)
 	TMPDIR=$(mktemp -d ${TMPDIR}.XXX)
 else
 	echo running reduction outside of a visit
@@ -60,11 +66,12 @@ ln -s $MOML $WORKSPACEMOML
 # /dls_sw/apps/DawnDiamond/master/builds-stable/stable-linux64/dawn -noSplash -application com.isencia.passerelle.workbench.model.launch -data $WORKSPACE -consolelog -os linux -ws gtk -arch $HOSTTYPE -vmargs -Dorg.dawb.workbench.jmx.headless=true -Dcom.isencia.jmx.service.terminate=false -Dmodel=$MODEL -Dxml.path=/scratch/ws/gda836_git/scisoft-ncd.git/uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/ncd_configuration.xml -Draw.path=/scratch/ws/gda836_git/scisoft-ncd.git/uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/i22-34820.nxs -Dpersistence.path=/scratch/ws/gda836_git/scisoft-ncd.git/uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/persistence_file.nxs -Doutput.path=/scratch/ws/junit-workspace/workflows/output
 #-data /tmp/foo \
 
-SCRIPT=$TMPDIR/qsub.script.$$
-cat >> qsub.script.$$ <<EOF
+SCRIPT=$TMPDIR/qsub.script
+cat >> $SCRIPT <<EOF
 #! /bin/sh
 
 ## set data reduction to started
+$ISPYBUPDATE reduction $DATACOLLID STARTED ""
 
 $DAWN -noSplash -application com.isencia.passerelle.workbench.model.launch \
 -data $WORKSPACE \
@@ -78,19 +85,39 @@ $DAWN -noSplash -application com.isencia.passerelle.workbench.model.launch \
 -Dpersistence.path=$PERSISTENCEFILE \
 -Doutput.path=$OUTPUTDIR
 
+for i in $OUTPUTDIR/results*.nxs ; do
+	GENERATEDFILE=\$i
+	break;
+done
 
-if test -n "$REDUCTIONOUTPUTFILE" ; then 
-	for i in $OUTPUTDIR/results*.nxs ; do
-		ln \$i $REDUCTIONOUTPUTFILE
-		break;
-	done
+if test -n "\$GENERATEDFILE" && test -r \$GENERATEDFILE ; then
+ : # all fine, but tell ISPyB later
+else 
+	# raise ISPyB error and exit
+	MESSAGE="ERROR cannot find generated reduction file for collection $DATACOLLID in $OUTPUTDIR"
+	$ISPYBUPDATE reduction $DATACOLLID FAILED $MESSAGE 
+	echo $MESSAGE  >&2
+	echo ABORTING. >&2
+	exit 1
 fi
 
-## check it worked and set data collection status in ispyb
-## abort here if failed
+if test -n "$REDUCTIONOUTPUTFILE" ; then 
+	ln \$GENERATEDFILE $REDUCTIONOUTPUTFILE
+	REDUCEDFILE="$REDUCTIONOUTPUTFILE"
+else
+	REDUCEDFILE=\$GENERATEDFILE
+fi
+
+# tell ispyb reduction worked and result is in \$REDUCEDFILE
+$ISPYBUPDATE reduction $DATACOLLID COMPLETE \$REDUCEDFILE
+
+module load edna/sas-local  ## we are on the cluster
 
 ## set analysis status started 
+$ISPYBUPDATE analysis $DATACOLLID STARTED ""
 ## run edna 
+run-sas-pipeline.py --data $REDUCEDFILE --nxsQ '/entry1/detector_result/q' --nxsData '/entry1/detector_result/data' --rMaxStart 50 --rMaxStop 600 --rMaxIntervals 25 --rMaxAbsTol 0.1 --mode fast --threads 10 --columns 10 --symmetry P6 --qmin 0.005 --qmax 0.3 --plotFit
+
 ## update ispyb
 EOF
 
