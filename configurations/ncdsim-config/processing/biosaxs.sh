8c9934b01e841e426b5f8619366755ac09c93ec7
Conor Lehane <xlw00930@ws141.diamond.ac.uk>
2014-03-20 12:28:43 +0000

    make sure running state is sent
    Conflicts:
    
    	configurations/ncdsim-config/processing/biosaxs.sh

--- configurations/ncdsim-config/processing/biosaxs.sh
+++ configurations/ncdsim-config/processing/biosaxs.sh
@@ -120,7 +120,7 @@ $ISPYBUPDATE analysis $DATACOLLID STARTED \"\"
 #module load edna/sas-local  ## we are on the cluster
 module load numpy/1.6.1
 mkdir $ANALYSISOUTPUT
-$ISPYBUPDATE analysis $DATACOLLID STARTED ""
+$ISPYBUPDATE analysis $DATACOLLID STARTED \"\"
 python $EDNAPYSCRIPT --filename \$REDUCEDFILE --backgroundFilename \$BACKGROUNDFILE --detector detector --dataCollectionId $DATACOLLID --outputFolderName $ANALYSISOUTPUT --threads 4 
 $ISPYBUPDATE analysis $DATACOLLID COMPLETE $ANALYSISOUTPUT
 
