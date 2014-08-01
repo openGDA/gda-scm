c4dd088d1598efa6a46de54f05831dc1d0caf660
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-08-01 14:17:48 +0100

    fix class
     * remove unnecessary code
     * make necessary items self so they can be used in the class
     * create constructor

--- plugins/uk.ac.gda.devices.bssc/scripts/createISPyBDataCollection.py
+++ plugins/uk.ac.gda.devices.bssc/scripts/createISPyBDataCollection.py
@@ -1,5 +1,10 @@
 import h5py, sys
 class ispybDataCollection:
+	def __init__(self):
+		host = "ispybb-test.diamond.ac.uk"
+		webServiceName = "ToolsForCollectionWebService"
+		self.createWebServiceClient(host, webServiceName)
+
 	def createWebServiceClient(self, host, webServiceName):
 		from suds.client import Client
 		from suds.transport.http import HttpAuthenticated
@@ -10,34 +15,21 @@ class ispybDataCollection:
 		username = ispybbUser()
 		userPassword = ispybbPassword()
 		httpAuthenticatedWebService = HttpAuthenticated(username=username, password=userPassword)
-		client = Client(URL, transport=httpAuthenticatedWebService)
-		client.options.cache.clear()
-		return client
-	def getH5File(self, fileName):
-		import h5py
-		return h5py.File(fileName,'r')
-	
-	def setCollectionValues(self, dataCollection, collectionValues):
+		self.client = Client(URL, transport=httpAuthenticatedWebService)
+		self.client.options.cache.clear()
+
+	def setCollectionValues(self, collectionValues):
 		for (key,value) in collectionValues.items():
-			dataCollection.__setattr__(key,value)
-		return dataCollection
+			self.collection.__setattr__(key,value)
+		return self.collection
 	
 	def createDataCollection(self):
-		collection = client.factory.create('dataCollectionWS3VO')
+		self.collection = self.client.factory.create('dataCollectionWS3VO')
 
-	def storeCollection(self, dataCollection, client):
-		returned = client.service.storeOrUpdateDataCollection(dataCollection)
+	def storeCollection(self):
+		returned = self.client.service.storeOrUpdateDataCollection(self.collection)
 		return returned
 
-
-#main body
-sys.exit(1)
-host = "ispybb-test.diamond.ac.uk"
-webServiceName = "ToolsForCollectionWebService"
-client = createWebServiceClient(host, webServiceName)
-collection = client.factory.create('dataCollectionWS3VO')
-fileIn="/dls/b21/data/2014/cm4976-3/b21-12433.nxs"
-h5File = getH5File(fileIn)
-values = getDataFromH5File(h5File)
-collection = setCollectionValues(collection, values)
-print storeCollection(collection, client)
+if __name__ == '__main__':
+	print "this is not meant to be run standalone, exiting"
+	sys.exit(1)
