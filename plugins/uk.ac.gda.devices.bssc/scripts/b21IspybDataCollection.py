c626f764deaafae6b8c43ad5de91bd98cabd8209
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-08-04 10:30:52 +0100

    refactor a few more methods to store info

--- plugins/uk.ac.gda.devices.bssc/scripts/b21IspybDataCollection.py
+++ plugins/uk.ac.gda.devices.bssc/scripts/b21IspybDataCollection.py
@@ -15,16 +15,6 @@ def getDataFromH5File(h5In, reducedFile):
 	assert h5In[energyPath].attrs[energyUnitAttribute]=="keV"
 	map["wavelength"] = 12.398425/energyInKev
 
-	#TODO transmission will be 100% if D3 filter positioner is in the "Scatter Diode" position. if not in this position, we should put in NaN, if Oracle can handle it
-	transmission = 100
-	map["transmission"] = transmission
-
-	#TODO get from S5 X and Y sizes in EPICS. beam is assumed to be parallel from there
-	beamSizeX = 4.0925
-	beamSizeY = 0.8195
-	map["beamSizeAtSampleX"] = beamSizeX
-	map["beamSizeAtSampleY"] = beamSizeY
-
 	qPath = "/entry1/detector_result/q"
 	qDataInvNm = reducedFile[qPath]
 	assert reducedFile[qPath].attrs["units"]=="Angstrom^-1"
@@ -43,11 +33,6 @@ def getDataFromH5File(h5In, reducedFile):
 	
 	map["comments"] = "no comment"
 
-	map["xtalSnapshotFullPath1"] = "/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_5_crop.png"
-	map["xtalSnapshotFullPath2"] = "/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_4_crop.png"
-	map["xtalSnapshotFullPath3"] = "/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_1_crop.png"
-	map["xtalSnapshotFullPath4"] = "/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_3_crop.png"
-
 	#TODO need some explanation on what the following fields are for	
 	map["dataCollectionNumber"] = 12433
 	map["imageDirectory"] = "/dls/b21/data/2014/cm4962-3/.ispyb/testDir/"
@@ -56,12 +41,38 @@ def getDataFromH5File(h5In, reducedFile):
 
 	return map
 
+def storeTransmission(map, d3FilterPositioner):
+	#TODO transmission will be 100% if D3 filter positioner is in the "Scatter Diode" position. if not in this position, we should put in NaN, if Oracle can handle it
+	assert d3FilterPositioner=="Scatter Diode"
+	transmission = 100
+	map["transmission"] = transmission
+	return map
+
+def storeBeamSize(map, beamSizeXMM, beamSizeYMM):
+	#TODO these values should be from S5 X and Y sizes in EPICS. beam is assumed to be parallel from there
+	map["beamSizeAtSampleX"] = beamSizeXMM
+	map["beamSizeAtSampleY"] = beamSizeYMM
+	return map
+
+def storeImages(map, summaryImageFile1, snapshotImageFile1, snapshotImageFile2, snapshotImageFile3):
+	map["xtalSnapshotFullPath1"] = summaryImageFile1
+	map["xtalSnapshotFullPath2"] = snapshotImageFile1
+	map["xtalSnapshotFullPath3"] = snapshotImageFile2
+	map["xtalSnapshotFullPath4"] = snapshotImageFile3
+	return map
+
 import h5py
 fileIn="/dls/b21/data/2014/cm4976-3/b21-12433.nxs"
 reducedFileIn="/dls/b21/data/2014/cm4976-3/b21-12434.nxs.22967.reduction/output/background_b21-12433_detector_260614_132314.nxs"
 h5File = h5py.File(fileIn,'r')
 reducedFile = h5py.File(reducedFileIn, 'r')
 values = getDataFromH5File(h5File, reducedFile)
+values = storeTransmission(values, "Scatter Diode") #actual value as of 2014-07-31
+values = storeBeamSize(values, 4.0925, 0.8195) #actual values (in MM) as of 2014-07-31
+values = storeImages(values, "/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_5_crop.png",
+	"/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_4_crop.png",
+	"/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_1_crop.png",
+	"/dls/b21/data/2014/cm4962-3/.ispyb/testDir/testPrefix/ispybb_multiple_curves_20140731_3_crop.png")
 import ispybDataCollection
 dc=ispybDataCollection.ispybDataCollection()
 dc.setCollectionValues(values)
