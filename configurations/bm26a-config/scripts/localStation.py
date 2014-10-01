d66eb4f68384fa5af22d0213f3067d61d4785694
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2014-10-01 15:08:49 +0100

    XAS-172_refactor: remove redundant ExafsScritpController from AsciiDataWriter

--- configurations/bm26a-config/scripts/localStation.py
+++ configurations/bm26a-config/scripts/localStation.py
@@ -8,14 +8,14 @@ from uk.ac.gda.server.exafs.scan import XasScan
 from gda.factory import Finder
 from gda.configuration.properties import LocalProperties
 from gda.jython.scriptcontroller.logging import LoggingScriptController
-from gda.scan import ScanBase#this is required for skip current repetition to work BLXVIIIB-99
+from gda.scan import ScanBase #this is required for skip current repetition to work BLXVIIIB-99
 from gda.data.scan.datawriter import NexusDataWriter
 from gda.device.detector.xspress import XspressDetectorConfiguration
 #from gdascripts.metadata.metadata_commands import meta_add,meta_ll,meta_ls,meta_rm, meta_clear_alldynamical
 
 XASLoggingScriptController = Finder.getInstance().find("XASLoggingScriptController")
 commandQueueProcessor = Finder.getInstance().find("commandQueueProcessor")
-#ExafsScriptObserver = Finder.getInstance().find("ExafsScriptObserver")
+ExafsScriptObserver = Finder.getInstance().find("ExafsScriptObserver")
 
 xspressConfig = XspressDetectorConfiguration(xspress1system, ExafsScriptObserver)
 print "configured xspress"
@@ -27,7 +27,7 @@ metashop = Finder.getInstance().find("metashop")
 
 detectorPreparer = BM26aDetectorPreparer(bragg1, xspressConfig)
 samplePreparer = BM26aSamplePreparer(sampleStage, cryoStage)
-outputPreparer = BM26aOutputPreparer(datawriterconfig)
+outputPreparer = BM26aOutputPreparer(datawriterconfig, metashop)
 xas = XasScan(BM26aBeamlinePreparer(), detectorPreparer, samplePreparer, outputPreparer, commandQueueProcessor, XASLoggingScriptController, datawriterconfig, original_header, bragg1, metashop, False)
 xanes = xas
 
