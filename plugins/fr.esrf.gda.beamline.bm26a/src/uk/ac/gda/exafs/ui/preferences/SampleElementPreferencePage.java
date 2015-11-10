9e84ca509d103d50ade0d314e86d8c18d5c5ee91
Colin Palmer <colin.palmer@diamond.ac.uk>
2015-11-10 11:47:12 +0000

    Rename org.eclipse.richbeans.api.reflection to .binding

--- plugins/fr.esrf.gda.beamline.bm26a/src/uk/ac/gda/exafs/ui/preferences/SampleElementPreferencePage.java
+++ plugins/fr.esrf.gda.beamline.bm26a/src/uk/ac/gda/exafs/ui/preferences/SampleElementPreferencePage.java
@@ -22,10 +22,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.richbeans.api.binding.IBeanController;
+import org.eclipse.richbeans.api.binding.IBeanService;
 import org.eclipse.richbeans.api.event.ValueAdapter;
 import org.eclipse.richbeans.api.event.ValueEvent;
-import org.eclipse.richbeans.api.reflection.IBeanController;
-import org.eclipse.richbeans.api.reflection.IBeanService;
 import org.eclipse.richbeans.widgets.selector.VerticalListEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
