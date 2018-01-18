addfb384f358c375dc8877fd76f7514b5c7cb646
Matthew Webber <matthew.webber@diamond.ac.uk>
2018-01-18 11:27:25 +0000

    Remove ncdsim-config; no longer required

--- configurations/ncdsim-config/scripts/installgridscan.py
+++
@@ -1,7 +0,0 @@
-import gridscan
-from gda.device.scannable.scannablegroup import ScannableGroup
-gridxy=ScannableGroup()
-gridxy.setName("gridxy")
-gridxy.setGroupMembers([x,y])
-gridxy.configure()
-ncdgridscan = gridscan.Grid("Camera View", "Mapping Grid", None, gridxy, ncddetectors)
